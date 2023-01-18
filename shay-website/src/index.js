d273792265fbe9865463aa93bdddc15f3f1b6086
shaygyawali <shaygyawali@gmail.com>
2023-01-18 03:27:42 -0500

    update

--- shay-website/src/index.js
+++ shay-website/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
 
-const root = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
