93c2205ac485954de831a8b9af41d0c9aa7efe09
shaygyawali <shaygyawali@gmail.com>
2023-01-27 02:42:30 -0500

    frontend progress

--- shay-website/src/App.js
+++ shay-website/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import CustomNav from './CustomNav'
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import pointer from './pointer.png'; // Tell webpack this JS file uses this image
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YERRR <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <CustomNav></CustomNav>
+    <div className ="hero">
+    <p className="title"> Hi! I'm Shay, a Student.</p> 
+    <p className="subTitle">As a quick, dedicated learner and lover of innovative tech, I’ve been applying my skills _________: </p>
+    <img className="downPointer" src={pointer} alt="pointer" />
+  
+    </div>   
+  </div>
   );
 }
 
---
+++ shay-website/src/CustomNav.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function CustomNav() {
+  return (
+    <>
+      {['sm'].map((expand) => (
+        <Navbar key={expand} bg="white" expand={expand} className="mb-3" sticky="top">
+          <Container fluid>
+            <Navbar.Brand href="#">Shay Gyawali</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavDropdown
+                    title="Work"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item href="#action3"> LMAOO </NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Timey
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Github
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <Nav.Link href="#action1">Contact</Nav.Link>
+                  <Nav.Link href="#action2">Resume</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default CustomNav;
